707acc434b41611c80a4f43b687f707f87701bf0
Ashley <ashleynjeri7@gmail.com>
2022-10-23 18:50:32 +0300

    passed tests

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat 
+    attr_accessor :name
+
+    def meow 
+        puts 'meow!'
+    end
+end
+
+
+# Test 
+#{instantiates a new cat
+#receives attr_accessor
+#has a name
+#is able to meow}
